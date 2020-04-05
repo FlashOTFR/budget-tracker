5253b8951bf3413a0657ba78f03cc1180c8e3f52
Randall Burgess <randallallenburgess@gmail.com>
2020-04-05 13:43:41 -0700

    added mlab for deployment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require(`express`);
 const mongoose = require(`mongoose`);
 
 const PORT = process.env.PORT || 3000;
-
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb: //localhost/mongoHeadlines";
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
